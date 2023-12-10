8d1502ed669a28a00e263a7bd3a87560804e51f4
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2023-12-10 10:22:07 +0100

    Fit the number of paths distribution to Gamma

---
+++ computeNPathThreshold.R
@@ -0,0 +1,43 @@
+# Install packages if not present
+packageList <- c("fitdistrplus", "ggplot2", "getopt")
+newPackages <- packageList[! (packageList %in% installed.packages()[,"Package"])]
+if (length(newPackages)) install.packages(newPackages)
+
+# Parse args
+spec <- matrix(c('help',   'h', 0, "logical",
+                 'input',  'i', 1, "character",
+                 'output', 'o', 1, "character"),
+               byrow = TRUE, ncol = 4)
+opt <- getopt::getopt(spec)
+
+# Print usage if requested
+if (! is.null(opt$help)) {
+  cat(getopt::getopt(spec, usage = TRUE))
+  q(status = 1)
+}
+
+# Check compulsory parameters
+if (is.null(opt$input)) {
+  stop("Missing input file.")
+}
+if (is.null(opt$output)) {
+  stop("Missing output file.")
+}
+
+# Parse input file
+d <- read.table(opt$input, col.names = c("chr", "start", "end", "id", "n.diff.paths", "strand"))
+n <- max(d$n.diff.paths)
+
+# Fit Gamma distribution, and find thresholds
+fitG <- fitdistrplus::fitdist(d$n.diff.paths, "gamma")
+threshold <- qgamma(0.05, shape = fitG$estimate[["shape"]], rate = fitG$estimate[["rate"]])
+print(paste0("Estimated threshold: ", threshold))
+
+# Plot distribution
+p <- ggplot2::ggplot(d, ggplot2::aes(n.diff.paths)) +
+	ggplot2::geom_density(adjust = 2, color = "black") +
+	ggplot2::stat_function(fun=dgamma, n = n, args=list(shape = fitG$estimate[["shape"]], rate = fitG$estimate[["rate"]]), linetype = "dashed", color = "darkgray") +
+	ggplot2::xlab("# paths") +
+	ggplot2::ylab("Density") +
+        ggplot2::geom_vline(xintercept = threshold, linetype = "dashed", color = "darkgray")
+ggplot2::ggsave(opt$output, p)
