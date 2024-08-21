b0061c564e643e87dfab98b672c85fa003f3bb9a
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2024-08-21 14:08:36 +0200

    Adapt to Jaccard

--- getExtremes.R
+++ getExtremes.R
@@ -34,63 +34,58 @@ if (is.null(opt$input)) {
 }
 
 # Parse input file
-d <- read.table(opt$input, col.names = c("chr", "start", "end", "id", "distance", "strand"))
-d$nDist <- as.integer(round(d$distance))
+input <- read.table("/Scratch/mazytnicki/PanSel/Results/MGC/10000/chr_all_hprc-v1.1-mc-chm13-full_GRCh38.0.chrall.bed", col.names = c("chr", "start", "end", "id", "index", "strand"))
+
+# Transform to log, remove zeros (hopefully, few)
+d <- input$index
+d <- d[d > 0]
+d <- -log(d)
+d <- d[d > 0]
 
 # Compute mode
-m <- as.numeric(names(which.max(as.list(table(d$nDist)))))
+m <- as.numeric(names(sort(-table(round(d, 4))))[1])
+m1 <- 1.1 * m
 
-lim1  <- max(5, m * 2)
-lim2  <- 0
-maxX1 <- m * 5
-maxX2 <- maxX1 * 5
-
-ints <- data.frame(left = d$nDist, right = d$nDist)
-# Estimate negative binomial with leftmost part of the distribution
-int1 <- ints
-int1$left[d$nDist > lim1] <- lim1
-int1$right[d$nDist > lim1] <- NA
-df1 <- d[d$nDist <= lim1, ]
-f1  <- fitdistrplus::fitdistcens(int1, "nbinom")
-threshold1 <- qnbinom(opt$pvalue1, size = f1$estimate[["size"]], mu = f1$estimate[["mu"]])
-threshold2 <- qnbinom(1 - opt$pvalue2, size = f1$estimate[["size"]], mu = f1$estimate[["mu"]])
+# Estimate normal with leftmost part of the distribution
+d1 <- d[d <= m1]
+d2 <- 2 * m1 - d1
+d1 <- c(d1, d2)
+f1 <- MASS::fitdistr(d1, "normal")
+threshold1 <- qnorm(opt$pvalue1, mean = f1$estimate[["mean"]], sd = f1$estimate[["sd"]])
 print(f1)
-print(threshold1)
+print(exp(-threshold1))
 
-# Estimate log normal with the positive part of distribution
-f2 <- fitdistrplus::fitdist(d$nDist[d$nDist > 0], "lnorm")
-print(f2)
+# Estimate log normal with the whole distribution
+f2 <- MASS::fitdistr(d, "lognormal")
 threshold2 <- qlnorm(1 - opt$pvalue2, meanlog = f2$estimate[["meanlog"]], sdlog = f2$estimate[["sdlog"]])
-print(threshold2)
-if (maxX2 < threshold2) {
-  maxX2 <- threshold2 * 1.2
-}
+print(f2)
+print(exp(-threshold2))
 
-df3 <- d[d$nDist <= maxX2, ]
+maxX <- threshold2 * 1.2
 
 # Print outliers (most conserved, least conserved) in BED format
 if (! is.null(opt$output1)) {
-  output1 <- d[d$nDist <= threshold1, ]
+  output1 <- input[input$index >= exp(-threshold1), ]
   write.table(output1, file = opt$output1, quote = FALSE, sep = "\t", col.names = FALSE, row.names = FALSE)
 }
 if (! is.null(opt$output2)) {
-  output2 <- d[d$nDist >= threshold2, ]
+  output2 <- input[input$index <= exp(-threshold2), ]
   write.table(output2, file = opt$output2, quote = FALSE, sep = "\t", col.names = FALSE, row.names = FALSE)
 }
 
 # Plot
 if (! is.null(opt$plot)) {
-  f <- function(.x, size, mu, meanlog, sdlog) {
-    return((dnbinom(round(.x), size = size, mu = mu) + dlnorm(.x, meanlog = meanlog, sdlog = sdlog)) / 2)
+  f <- function(.x, mean, sd, meanlog, sdlog) {
+    return((dnorm(.x, mean = mean, sd = sd) + dlnorm(.x, meanlog = meanlog, sdlog = sdlog)) / 2)
   }
-  p <- ggplot2::ggplot(df3, ggplot2::aes(nDist)) + 
-    ggplot2::geom_freqpoly(ggplot2::aes(y = ggplot2::after_stat(density)), binwidth = 1, color = "black") +
-    ggplot2::stat_function(fun = f, n = maxX2 + 1, args = list(size = f1$estimate[["size"]], mu = f1$estimate[["mu"]], meanlog = f2$estimate[["meanlog"]], sdlog = f2$estimate[["sdlog"]]), linetype = "dashed", color = "darkgreen") +
+  p <- ggplot2::ggplot(data.frame(data = d), ggplot2::aes(data)) +
+    ggplot2::geom_freqpoly(ggplot2::aes(y = ggplot2::after_stat(density)), binwidth = 0.0001) +
+    ggplot2::stat_function(fun = f, n = 10000, args = list(mean = f1$estimate[["mean"]], sd = f1$estimate[["sd"]], meanlog = f2$estimate[["meanlog"]], sdlog = f2$estimate[["sdlog"]]), color = "darkgreen", linetype = "dashed") +
     ggplot2::geom_vline(xintercept = threshold1, linetype = "dotted", color = "darkblue") +
     ggplot2::geom_vline(xintercept = threshold2, linetype = "dotted", color = "darkred") +
-    ggplot2::xlab("Edit distance") +
-    ggplot2::xlim(0, maxX2)
-    ggplot2::ggsave(opt$plot, p)
+    ggplot2::xlab("Log Jaccard index") +
+    ggplot2::xlim(0, maxX)
+  ggplot2::ggsave(opt$plot, p)
 }
 
 q(status = 0)
